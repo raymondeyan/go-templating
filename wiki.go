8ad9890447b098c77061dc119f5cc3e57006f29c
raymondeyan <raymondyan777@gmail.com>
2021-12-21 12:37:41 -0800

    init

---
+++ wiki.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+}
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return os.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, e := os.ReadFile(filename)
+	if e != nil {
+		return nil, e
+	}
+	return &Page{Title: title, Body: body}, nil
+}
